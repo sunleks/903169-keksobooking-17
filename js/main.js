7134e228d784f37cbb838058546ee5be1fe98ed4
sunleks@users <67sasha1996@mail.ru>
2019-06-10 23:20:34 +0300

    Доработать

--- js/main.js
+++ js/main.js
@@ -10,39 +10,46 @@ map.classList.remove('map--faded');
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
 var imgPin =  pin.querySelector('img');
 
-var pins = document.querySelector('.map__pins')
+var mapPins = document.querySelector('.map__pins')
 
 
-  var arrayObj = [
-    {
+  var arrayObj = {
     "author": {
-      "avatar": 'img/avatars/user0' + i + '.png'
-      }
-    },
-    {
+      "avatar": 'img/avatars/user0.png'
+      },
     "offer": {
       "type": ['palace', 'flat', 'house', 'bungalo']
-      }
-    },
-    {
+      },
     "location": {
-      "x": getRandomInRange(0, 600),
-      "y": getRandomInRange(130, 160)
+      "x": '',/*getRandomInRange(0, 600)*/
+      "y": ''/*getRandomInRange(130, 160)*/
       }
-    }
-  ];
+  };
 
-for (var i = 0; i <= 8; i++) {
+for (var i = 1; i <= 8; i++) {
   var pinsElement = pin.cloneNode(true);
-  pinsElement.style.left = arrayObj[2].location.x;
-  pinsElement.style.top = arrayObj[2].location.y;
+
+  /*Присваиваиваем раномное число координатам х и y*/
+  arrayObj.location.x = getRandomInRange(0, 600);
+  arrayObj.location.y = getRandomInRange(130, 160);
+  /*Добавляем рандомному числу px*/
+  pinsElement.style.left = arrayObj.location.x + 'px';
+  pinsElement.style.top = arrayObj.location.y + 'px';
 
   var pinsElementImgPin = imgPin.cloneNode(true);
-  pinsElementImgPin.src = arrayObj[0].author.avatar;
+
+  /*Перебираем аватары пользователей*/
+  arrayObj.author.avatar = 'img/avatars/user0' + i + '.png';
+  pinsElementImgPin.src = arrayObj.author.avatar;
+
   console.log(pinsElementImgPin.src);
+  /*Создаём переменную, которая будет брать раномные числа из диапазона от 0 до 3 и присваиваем alt
+  рандомное значение*/
+  var rand = getRandomInRange(0, arrayObj.offer.type.length - 1);
+  pinsElementImgPin.alt = arrayObj.offer.type[rand];
 
 
-  pins.appendChild(pinsElement);
-  pins.appendChild(pinsElementImgPin);
+  mapPins.appendChild(pinsElement);
+  mapPins.appendChild(pinsElementImgPin);
 };
 
