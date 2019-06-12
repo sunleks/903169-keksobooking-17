f10cf44474d1be68c07597cd8d8299a39ebcd9c7
sunleks@users <67sasha1996@mail.ru>
2019-06-12 15:48:07 +0300

    Travis

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-var mapPins = document.querySelector('.map__pins')
+var mapPins = document.querySelector('.map__pins');
 
 function getRandomInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 var MOCK = {
-  "author": {
+  'author': {
     "avatar": 'img/avatars/user0.png'
   },
-  "offer": {
-    "type": ['palace', 'flat', 'house', 'bungalo']
+  'offer': {
+    'type': ['palace', 'flat', 'house', 'bungalo']
   },
-  "location": {
-    "x": '',
-    "y": ''
+  'location': {
+    'x': '',
+    'y': ''
   },
-  "header": {
-    "name": ['лучшеее жильё', 'замечательное жильё', 'пойдёт', 'горящее предложение']
+  'header': {
+    'name': ['лучшеее жильё', 'замечательное жильё', 'пойдёт', 'горящее предложение']
   }
 };
 
@@ -30,27 +30,25 @@ var getDataMass = function () {
   var arr = [];
   for (var i = 0; i < 8; i++) {
     arr [i] = {
-      "author": {
-        "avatar": 'img/avatars/user0' + parseInt(i + 1) + '.png'
+      'author': {
+        'avatar': 'img/avatars/user0' + parseInt(i + 1) + '.png'
       },
-      "offer": {
+      'offer': {
         "type": MOCK.offer.type[getRandomInRange(0, MOCK.offer.type.length - 1)]
       },
-      "location": {
-        "x": getRandomInRange(0, 1150),
-        "y": getRandomInRange(130, 630)
+      'location': {
+        'x': getRandomInRange(0, 1150),
+        'y': getRandomInRange(130, 630)
       },
-      "header": {
-        "name": MOCK.header.name[getRandomInRange(0, MOCK.header.name.length - 1)]
+      'header': {
+        'name': MOCK.header.name[getRandomInRange(0, MOCK.header.name.length - 1)]
       }
     }
   }
   return arr;
 };
 
-
 var data = getDataMass();
-console.log(data);
 
 for (var i = 0; i < data.length; i++) {
   var element = pin.cloneNode(true);
