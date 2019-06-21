a7c9774a12574eb1f848d348ff2ca0b47db4b2bd
sunleks@users <67sasha1996@mail.ru>
2019-06-21 22:10:43 +0300

    переделал валидацию формы

--- js/main.js
+++ js/main.js
@@ -77,9 +77,12 @@ var enabledElement = function (collection) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < data.length; i++) {
-  fragment.appendChild(createPin(data[i]));
-}
+var createPins = function () {
+  for (var i = 0; i < data.length; i++) {
+    fragment.appendChild(createPin(data[i]));
+  }
+};
+createPins();
 
 var renderPins = function (node, elements) {
   node.appendChild(elements);
@@ -100,62 +103,34 @@ pinMain.addEventListener('mouseup', function () {
 
 var validType = document.querySelector('#type');
 var validPrice = document.querySelector('#price');
-var validOption = validType.querySelectorAll('option'); /* Поиск всех option в теге select с id = #price*/
-var giveDataAttribute = function (value, min, placeholder) {  /*Функция, которой передаются параметры и она их записывает в value и placeholder */
-  if (validType.value === value) {
-    validPrice.min = min;
-    validPrice.placeholder = placeholder;
+var getHousePrice = function () {
+  switch (validType.value) {
+    case 'bungalo':
+      return 0;
+    case 'flat':
+      return 1000;
+    case 'house':
+      return 5000;
+    case 'palace':
+      return 10000;
+    default:
+      return 1000;
   }
 };
 
-var getTypeOption = function () { /* Функция, которая получает данные из select с id = #type, option  и записывает в массив*/
-  var arr = [];
-  for (var i = 0; i < validOption.length; i++) {
-    arr[i] = validOption[i].value;
-  }
-  return arr;
-};
-
-var priceNightValue = [0, 1000, 5000, 10000]; /* Создал массив для значений value*/
-var priceNightPlaceholder = [0, 1000, 5000, 10000];/* Создал массив для значений placeholder*/
-
-giveDataAttribute('flat', 1000, 1000);/* По умолчанию стоит это значение  */
-
-validType.addEventListener('change', function () { /*Событие, которое следит за изменениями в поле select с id = #type и передаёт значения */
-  for (var i = 0; i < validOption.length; i++) {
-    giveDataAttribute(getTypeOption()[i], priceNightValue[i], priceNightPlaceholder[i]);
-  }
+validType.addEventListener('change', function () {
+  var minPrice = getHousePrice();
+  validPrice.setAttribute('min', minPrice);
+  validPrice.setAttribute('placeholder', minPrice);
 });
 
 var timein = document.querySelector('#timein');
 var timeout = document.querySelector('#timeout');
-var getTimeOption = function (data) { /* Функция, которая получает параметр Select. Записывает значения Option в массив  возращает массив */
-  var arr = [];
-  for (var i = 0; i < data.querySelectorAll('option').length; i++) {
-    arr[i] = data.querySelectorAll('option')[i].value;
-  }
-  return arr;
-};
-
-var giveDataTimein = function (value) {/* Функиция, которая сравнивает option.value со значением value и сохраняет значение value  в другой input*/
-  if (timein.value === value) {
-    timeout.value = timein.value;
-  }
-};
-var giveDataTimeout = function (value) {/* Функиция, которая сравнивает option.value со значением value и сохраняет значение value  в другой input*/
-  if (timeout.value === value) {
-    timein.value = timeout.value;
-  }
-};
 
 timein.addEventListener('change', function () {
-  for (var i = 0; i < getTimeOption(timein).length; i++) {
-    giveDataTimein(getTimeOption(timein)[i]);
-  }
+  timeout.value = timein.value;
 });
 
 timeout.addEventListener('change', function () {
-  for (var i = 0; i < getTimeOption(timeout).length; i++) {
-    giveDataTimeout(getTimeOption(timeout)[i]);
-  }
+  timein.value = timeout.value;
 });
