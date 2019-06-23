80bdb41b2218f8299fea26709c7140e61d47c674
sunleks@users <67sasha1996@mail.ru>
2019-06-23 21:12:06 +0300

    pin move

--- js/main.js
+++ js/main.js
@@ -88,13 +88,6 @@ var renderPins = function (node, elements) {
   node.appendChild(elements);
 };
 
-pinMain.addEventListener('click', function () {
-  map.classList.remove('map--faded');
-  form.classList.remove('ad-form--disabled');
-  enabledElement(formFieldsets);
-  renderPins(mapPins, fragment);
-});
-
 address.value = parseInt(pinMain.style.left, 10) + ', ' + parseInt(pinMain.style.top, 10);
 
 pinMain.addEventListener('mouseup', function () {
@@ -118,8 +111,12 @@ var getHousePrice = function () {
   }
 };
 
+var minPrice = getHousePrice();
+selectPrice.setAttribute('min', minPrice);
+selectPrice.setAttribute('placeholder', minPrice);
+
 selectType.addEventListener('change', function () {
-  var minPrice = getHousePrice();
+  minPrice = getHousePrice();
   selectPrice.setAttribute('min', minPrice);
   selectPrice.setAttribute('placeholder', minPrice);
 });
@@ -134,3 +131,55 @@ timein.addEventListener('change', function () {
 timeout.addEventListener('change', function () {
   timein.value = timeout.value;
 });
+///////////////////////////////////
+
+pinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  
+  map.classList.remove('map--faded');
+  form.classList.remove('ad-form--disabled');
+  enabledElement(formFieldsets);
+  renderPins(mapPins, fragment);
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+    pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+
+    if ((pinMain.offsetLeft - shift.x) < -30) {
+      pinMain.style.left = -30 + 'px';
+    } else if ((pinMain.offsetLeft - shift.x) > 1150) {
+      pinMain.style.left = 1150 + 'px';
+    };
+    if ((pinMain.offsetTop - shift.y) < 130) {
+      pinMain.style.top = 130 + 'px';
+    } else if ((pinMain.offsetTop - shift.y) > 630) {
+      pinMain.style.top = 630 + 'px';
+    };
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+}); 
